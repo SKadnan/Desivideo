3309bca4ae45521002b9d2b5509d98e1de376db8
SKadnan <88537815+SKadnan@users.noreply.github.com>
2026-09-05 13:33:28 +0500

    Add metadata and Google verification to page.js

---
+++ app/page.js
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: 'Desivideo',
+  verification: {
+    google: '8LAED4d9N7d1IA9Ahv3ScVBkAKeb2ZXMk5XVX2ORH98',
+  },
+};
+
+export default function Home() {
+  return (
+    <html>
+      <head>
+        <meta name="google-site-verification" content="8LAED4d9N7d1IA9Ahv3ScVBkAKeb2ZXMk5XVX2ORH98" />
+      </head>
+      <body>
+        <h1>Desivideo Active</h1>
+      </body>
+    </html>
+  );
+}
