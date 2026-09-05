035a30bc8343d181c9b6c3323716d0aad4bdb59e
SKadnan <88537815+SKadnan@users.noreply.github.com>
2026-09-05 13:29:14 +0500

    Add RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Desivideo',
+  verification: {
+    google: '8LAED4d9N7d1IA9Ahv3ScVBkAKeb2ZXMk5XVX2ORH98',
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
