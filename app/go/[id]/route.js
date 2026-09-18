18cedc68e098ce3b6d11a4964876b5037221311a
SKadnan <88537815+SKadnan@users.noreply.github.com>
2026-09-18 12:41:15 +0500

    Improve URL redirection logic in route.js
    Refactor GET function to improve URL handling and error checking.

--- app/go/[id]/route.js
+++ app/go/[id]/route.js
@@ -1,32 +1,36 @@
 import { NextResponse } from 'next/server';
 
-// Aapke saare target links
 const targetUrls = {
-  "doc1": "https://docs.google.com/document/d/YOUR_DOC_ID_1/edit",
-  "doc2": "https://docs.google.com/document/d/YOUR_DOC_ID_2/edit",
-  "link1": "https://i.urlxx335.com/?utm_source=Raees&utm_medium=SK"
+  doc1: 'https://docs.google.com/document/d/YOUR_DOC_ID_1/edit',
+  doc2: 'https://docs.google.com/document/d/YOUR_DOC_ID_2/edit',
+  link1: 'https://i.urlxx335.com/?utm_source=Raees&utm_medium=SK',
 };
 
 export async function GET(request, { params }) {
-  const resolvedParams = await params;
-  const id = resolvedParams.id;
+  const { id } = await params;
   const destination = targetUrls[id];
 
-  if (destination) {
-    // Incoming query parameters bhi forward honge
-    const { search } = new URL(request.url);
-    const finalUrl = `${destination}${search}`;
-
-    return NextResponse.redirect(finalUrl, {
-      status: 307,
-      headers: {
-        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-      },
-    });
+  if (!destination) {
+    return NextResponse.redirect(new URL('/', request.url), 307);
   }
 
-  // Agar link ID na mile toh homepage par redirect
-  return NextResponse.redirect(new URL('/', request.url), 307);
+  // Destination URL ko safely parse karein
+  const finalUrl = new URL(destination);
+
+  // Incoming parameters read karein
+  const incomingUrl = new URL(request.url);
+
+  // Incoming query parameters merge karein
+  for (const [key, value] of incomingUrl.searchParams) {
+    finalUrl.searchParams.set(key, value);
+  }
+
+  return NextResponse.redirect(finalUrl, {
+    status: 307,
+    headers: {
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+    },
+  });
 }
